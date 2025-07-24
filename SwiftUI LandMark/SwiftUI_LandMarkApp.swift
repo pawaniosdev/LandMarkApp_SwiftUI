cee8f765502839a5aaf2e85130c713f26b4a1c0f
pawaniosdev <pawanarsd@gmail.com>
2025-07-25 01:14:00 +0530

    Initial Commit

---
+++ SwiftUI LandMark/SwiftUI_LandMarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_LandMarkApp.swift
+//  SwiftUI LandMark
+//
+//  Created by Pawan kumar Verma on 25/07/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_LandMarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
